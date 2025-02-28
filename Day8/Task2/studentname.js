ed6a3daf9ec133e568c875a4b0fd662eebb09628
Varsha-hub-create <varshinisakthivel18@gmail.com>
2025-02-28 12:46:30 +0530

    studentname.js

---
+++ Day8/Task2/studentname.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const StudentName = () => {
+  return (
+    <div
+  style={{
+    fontSize: "20px",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: "20px",
+    paddingLeft: "630px"
+  }}
+>
+  Student Name: varshini
+</div>
+
+  );
+};
+
+export default StudentName;
