181f17356837d2bb045fcec143856cbaca613d7f
Varsha-hub-create <varshinisakthivel18@gmail.com>
2025-02-28 13:03:09 +0530

    styledname.js

---
+++ Day8/Task3/styledname.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const StudentName = () => {
+  return (
+    <div
+      style={{
+        fontSize: "24px",
+        fontWeight: "bold",
+        color: "blue", // Text color set to blue
+        textAlign: "center",
+        marginTop: "20px",
+        paddingLeft: "630px",
+      }}
+    >
+      Student Name: Varshini
+    </div>
+  );
+};
+
+export default StudentName;
